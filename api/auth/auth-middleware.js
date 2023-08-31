b9f81e71bf298bd018e54113e26252bb15e1cb61
Kenneth Fisher <kenandcrys@live.com>
2023-08-31 05:21:18 -0500

    commit

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -8,6 +8,14 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
+  if (req.session.user){
+    next()
+  } else {
+    next({
+      status: 401,
+      message: "You shall no pass!"
+    })
+  }
   // if (!req.session || !req.session.user) {
   //   return res.status(401).json({ message: "You shall not pass!" });
   // }
